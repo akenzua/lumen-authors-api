6fc2edfcc4c76dcac7bcc923b717f78c3e192b13
Akintade Ajibade <ajibade.akintade@gmail.com>
2021-02-01 14:38:24 +0000

    wrote the destroy function for AuthorController

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -65,5 +65,8 @@ class AuthorController extends Controller
 
     public function destroy($author){
 
+        $author = Author::findOrFail($author);
+        $author->delete();
+        return $this->successResponse($author);
     }
 }
