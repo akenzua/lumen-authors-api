4d3bd1897b8be8cb59fd0d6ab6db17f3addf0a34
Akintade Ajibade <ajibade.akintade@gmail.com>
2021-02-01 12:06:51 +0000

    created author controller

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index(){
+        
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function show($author){
+
+    }
+
+    public function update(Request $request, $author){
+
+    }
+
+    public function destroy($author){
+
+    }
+}
