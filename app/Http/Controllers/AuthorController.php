06ec1d5ccd326680167f139bbc8023838e8ee912
Akintade Ajibade <ajibade.akintade@gmail.com>
2021-02-01 12:43:30 +0000

    created success/error responses

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
+    use ApiResponser;
     /**
      * Create a new controller instance.
      *
